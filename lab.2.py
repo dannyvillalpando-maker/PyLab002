e6e10f63c87f4c7920e0347883763e60899c94a7
Danielvillalndo <dannyvillalpando@icloud.com>
2026-02-06 16:03:35 -0800

    finsihed lab 2

---
+++ lab.2.py
@@ -0,0 +1,38 @@
+name = "daniel"
+age = 14
+height = 5.11
+favorite_color = "blue"
+print (name)
+print (age)
+print(height)
+print (favorite_color)
+
+print (name,age ,height ,favorite_color)
+
+print (f"hello: {name} my is {favorite_color}!")
+
+from math import pi
+radius = 5
+circle_area = pi * radius **2
+
+
+
+import math
+print(math.sqrt(age), math.sin(height), math.cos(height))
+
+print('-'*80)
+
+print(age + 5)
+print(height - 4)
+print(age * height)
+print(height / 2)
+print(age % 3)
+print(age**2)
+
+print('-'*80)
+
+print("tempature conversion")
+temp_f = float(input(f"{name} Enter a tempature in Farenheit: "))
+celsius = (temp_f - 32) * (5/9)
+print("f",{celsius}, "c")
+
